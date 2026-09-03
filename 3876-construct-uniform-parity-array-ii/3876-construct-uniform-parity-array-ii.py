9b54588ae2e5c3f4f1a09d30c00ccc8bf3da6d55
Ranga Chandra Naga Venkata Chaitanya Kumar <142649050+chaitanyakumarAI@users.noreply.github.com>
2026-09-03 15:47:07 +0530

    Time: 7 ms (99.39%), Space: 35.3 MB (51.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,13 +1,7 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        onum = [x for x in nums1 if x % 2 == 1]
-        enum = [x for x in nums1 if x % 2 == 0]
-        if not onum or not enum:
-            return True
-            
-        min_odd = min(onum)
-        min_even = min(enum)
-        if min_odd<min_even:
+        m=min(nums1)
+        if m%2==1 or all(nums1[i]%2==0 for i in range(len(nums1))):
             return True
         else:
             return False
