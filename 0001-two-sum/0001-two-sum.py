b058127b41cd66688bd343c14b6e7b4f25d74eb5
Ranga Chandra Naga Venkata Chaitanya Kumar <142649050+chaitanyakumarAI@users.noreply.github.com>
2026-09-09 12:06:00 +0530

    Time: 0 ms (100%), Space: 20.5 MB (41.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,33 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        n=len(nums)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dnums={item:index for index,item in enumerate(nums)}
+        for i in range(len(nums)):
+            if target-nums[i] in dnums and i!=dnums[target-nums[i]]:
+                return [i,dnums[target-nums[i]]]
         
-        l=0
-        r=n-1
-        ret=[]
-        count={}
-        for i,e in enumerate(nums):
-            if e in count:
-                count[e].append(i)
-            else:
-                count[e]=[i]
-        print count
-        nums.sort()
-        while l<r:
-            if nums[l]+nums[r]==target:
-                ret.append(count[nums[l]][0])
-                if nums[l]==nums[r]:
-                    ret.append(count[nums[r]][1])
-                else:
-                    ret.append(count[nums[r]][0])
-                break
-            elif nums[l]+nums[r]<target:
-                l+=1
-            else:
-                r-=1
-        return ret
