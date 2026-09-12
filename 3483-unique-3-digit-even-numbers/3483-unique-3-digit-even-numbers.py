bcb6133ae7f4910dd2a1fa357891a02ab9565d56
Ranga Chandra Naga Venkata Chaitanya Kumar <142649050+chaitanyakumarAI@users.noreply.github.com>
2026-09-12 15:32:44 +0530

    Time: 17 ms (59.21%), Space: 19.3 MB (71.93%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -4,11 +4,12 @@ class Solution:
         count=0
         snums=set()
         for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    if i!=j and j!=k and i!=k and digits[i]!=0 and digits[k]%2==0:
-                        num=digits[i]*100+digits[j]*10+digits[k]
-                        if num not in snums:
-                            snums.add(num)
-                            count+=1
+            if digits[i]!=0:
+                for j in range(n):
+                    for k in range(n):
+                        if i!=j and j!=k and i!=k and digits[k]%2==0:
+                            num=digits[i]*100+digits[j]*10+digits[k]
+                            if num not in snums:
+                                snums.add(num)
+                                count+=1
         return count
