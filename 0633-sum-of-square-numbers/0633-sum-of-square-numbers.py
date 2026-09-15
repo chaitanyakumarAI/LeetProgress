3689810f80e4098258b72fcca30d9bfcad647802
Ranga Chandra Naga Venkata Chaitanya Kumar <142649050+chaitanyakumarAI@users.noreply.github.com>
2026-09-15 16:10:05 +0530

    Time: 67 ms (42.14%), Space: 19.3 MB (87.56%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        a=0
+        b=int(c**0.5)
+        while a<=b:
+            if a**2+b**2<c:
+                a+=1
+            elif a**2+b**2>c:
+                b-=1
+            else:
+                return True
+        return False
