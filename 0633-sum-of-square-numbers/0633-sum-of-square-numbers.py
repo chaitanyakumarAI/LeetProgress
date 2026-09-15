598d95be72f69832779d899f238c1490a1d5cc5c
Ranga Chandra Naga Venkata Chaitanya Kumar <142649050+chaitanyakumarAI@users.noreply.github.com>
2026-09-15 16:10:37 +0530

    Time: 90 ms (25.08%), Space: 19.4 MB (58.06%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -3,10 +3,11 @@ class Solution:
         a=0
         b=int(c**0.5)
         while a<=b:
-            if a**2+b**2<c:
-                a+=1
-            elif a**2+b**2>c:
+            if a**2+b**2>c:
                 b-=1
+            elif a**2+b**2<c:
+                a+=1
+            
             else:
                 return True
         return False
